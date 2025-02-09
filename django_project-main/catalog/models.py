14ade2cec5b9e6ff81d9c827a62f3f62e227b23e
Mirolity <murachev.00@gmail.com>
2025-02-09 12:32:51 +0300

    изменен шаблон главной страницы, добавлен код из дополнительного задания

--- django_project-main/catalog/models.py
+++ django_project-main/catalog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
@@ -12,11 +11,10 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
-
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='products/', verbose_name='Изображение')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
@@ -29,7 +27,6 @@ class Product(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
 
-
 class Feedback(models.Model):
     name = models.CharField(max_length=255, verbose_name='Имя')
     email = models.EmailField(verbose_name='Email')
@@ -41,3 +38,16 @@ class Feedback(models.Model):
     class Meta:
         verbose_name = 'Обратная связь'
         verbose_name_plural = 'Обратная связь'
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Имя')
+    email = models.EmailField(verbose_name='Email')
+    phone = models.CharField(max_length=50, verbose_name='Телефон', blank=True, null=True)  # Увеличили длину поля и добавили возможность быть пустым
+    message = models.TextField(verbose_name='Сообщение')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
--- django_project-main/catalog/views.py
+++ django_project-main/catalog/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Category, Product, Feedback
+from .models import Category, Product, Feedback, Contact
 from .forms import FeedbackForm
-
 
 def index(request):
     # Получение последних пяти товаров
     latest_products = Product.objects.all().order_by('-created_at')[:5]
+    # Получение двух последних добавленных товаров для каждой категории
+    categories = Category.objects.all()
+    latest_products_by_category = {
+        category: Product.objects.filter(category=category).order_by('-created_at')[:2]
+        for category in categories
+    }
     print(latest_products)  # Вывод последних пяти товаров в консоль
-    return render(request, 'main/index.html', {'latest_products': latest_products})
-
+    return render(request, 'main/index.html', {
+        'latest_products': latest_products,
+        'latest_products_by_category': latest_products_by_category
+    })
 
 def contact(request):
     if request.method == 'POST':
@@ -19,13 +26,13 @@ def contact(request):
             return HttpResponse('Спасибо за ваш отзыв!')
     else:
         form = FeedbackForm()
-    return render(request, 'main/contact.html', {'form': form})
 
+    contacts = Contact.objects.all()
+    return render(request, 'main/contact.html', {'form': form, 'contacts': contacts})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'catalog/product_detail.html', {'product': product})
-
 
 def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
